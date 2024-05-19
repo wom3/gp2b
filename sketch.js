1db5155df825decc32bf82a8f393f47fc95db21d
wom3 <wom2@student.london.ac.uk>
2024-05-19 18:57:04 +0500

    feat: Trees and Clouds

--- sketch.js
+++ sketch.js
@@ -32,6 +32,7 @@ function setup()
 
 	treePos_x = width/2;
 	treePos_y = height/2;
+    
     canyon = {
         x_pos: 0,
         width: 100 
@@ -41,6 +42,17 @@ function setup()
         x_pos: 100,
         y_pos: 100,
         size: 50
+    }
+    
+    mountain = {
+        x_pos: -120,
+        y_pos: 100
+    }
+    
+    cloud = {
+        x_pos: 0,
+        y_pos: 0,
+        size: 20
     }
 }
 
@@ -52,6 +64,23 @@ function draw()
 	fill(0, 155, 0);
 	rect(0, floorPos_y, height, width - floorPos_y); //draw some green ground
     
+    // Draw mountain
+    
+    fill(105, 105, 105)
+	quad(mountain.x_pos+380, mountain.y_pos+332, mountain.x_pos+480, mountain.y_pos+132, mountain.x_pos+580, mountain.y_pos+132, mountain.x_pos+680, mountain.y_pos+332)
+	fill(255)
+	triangle(mountain.x_pos+480, mountain.y_pos+132, mountain.x_pos+500, mountain.y_pos+102, mountain.x_pos+520, mountain.y_pos+132)
+	triangle(mountain.x_pos+500, mountain.y_pos+132, mountain.x_pos+530, mountain.y_pos+82, mountain.x_pos+560, mountain.y_pos+132)
+	triangle(mountain.x_pos+540, mountain.y_pos
+             +132, mountain.x_pos+560, mountain.y_pos+102, mountain.x_pos+580, mountain.y_pos+132)
+    
+    // Draw cloud
+    
+    fill(255)
+	ellipse(cloud.x_pos+200, cloud.y_pos + 150, 5*cloud.size)
+	ellipse(cloud.x_pos+250,cloud.y_pos+ 150,5*cloud.size)
+	ellipse(cloud.x_pos+300,cloud.y_pos+ 150, 4*cloud.size)
+    
     //2. Draw tree
     
     fill(165,42,42)
@@ -60,6 +89,16 @@ function draw()
     triangle(treePos_x-40,treePos_y+20,treePos_x+10,treePos_y-30,treePos_x+60,treePos_y+20)
     triangle(treePos_x-37,treePos_y,treePos_x+10,treePos_y-60,treePos_x+57,treePos_y)
     triangle(treePos_x-32,treePos_y-22,treePos_x+10,treePos_y-60,treePos_x+52,treePos_y-22)
+    
+    // Draw canyon
+    
+	fill(139, 69, 19);
+	quad(canyon.x_pos+100, canyon.width+332, canyon.x_pos+200, canyon.width +332, canyon.x_pos+220, canyon.width+476, canyon.x_pos+80, canyon.width+476);
+
+    
+    // Draw collectable
+    fill(255, 215, 0);
+	ellipse(collectable.x_pos+20, collectable.y_pos+310, collectable.size, collectable.size)
     
 	//1. Draw Game character
     
@@ -107,18 +146,7 @@ function draw()
     
     fill(128,0,0);
     quad(gameChar_x+3,gameChar_y-8,gameChar_x+6,gameChar_y-8,gameChar_x+9,gameChar_y,gameChar_x+2,gameChar_y)
-    
-    // Draw canyon
-    
-	fill(139, 69, 19);
-	quad(canyon.x_pos+200, canyon.width+332, canyon.x_pos+300, canyon.width +332, canyon.x_pos+320, canyon.width+476, canyon.x_pos+180, canyon.width+476);
 
-	noStroke();
-	fill(255);
-    
-    // Draw collectable
-    fill(255, 215, 0);
-	ellipse(collectable.x_pos+120, collectable.y_pos+310, collectable.size, collectable.size)
 
 }
 
